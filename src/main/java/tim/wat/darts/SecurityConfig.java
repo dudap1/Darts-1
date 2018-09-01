aa4d2b5d0a95b85565e50228fa25182f9a556db9
dudap1 <39228050+dudap1@users.noreply.github.com>
2018-09-02 00:19:39 +0200

    disable cors attempt3

--- src/main/java/tim/wat/darts/SecurityConfig.java
+++ src/main/java/tim/wat/darts/SecurityConfig.java
@@ -9,7 +9,6 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import tim.wat.darts.services.UserService;
 
 import javax.sql.DataSource;
@@ -47,6 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                .cors().disable()
                 .csrf().disable();
 
         http.authorizeRequests()
