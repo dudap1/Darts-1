f5a7193026e611d7bcf9f7f3d7b99462a1817f0d
dudap1 <39228050+dudap1@users.noreply.github.com>
2018-09-02 00:28:56 +0200

    disable cors attempt4

--- src/main/java/tim/wat/darts/SecurityConfig.java
+++ src/main/java/tim/wat/darts/SecurityConfig.java
@@ -2,6 +2,7 @@ package tim.wat.darts;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,11 +10,14 @@ import org.springframework.security.config.annotation.web.configuration.EnableWe
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import tim.wat.darts.services.UserService;
 
 import javax.sql.DataSource;
 
+@Configuration
 @EnableWebSecurity
+@CrossOrigin(allowCredentials="true")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -46,7 +50,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-                .cors().disable()
                 .csrf().disable();
 
         http.authorizeRequests()
