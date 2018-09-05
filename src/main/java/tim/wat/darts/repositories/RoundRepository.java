675d3c9bd962f6af73c8cc7cadbcfdadd9a3834d
Łukasz Gdula <lgdu@candf.com>
2018-09-05 14:08:04 +0200

    Revert "query change"
    This reverts commit 7770c440fda0f726b7cf534d7b256e00cf67a565.

--- src/main/java/tim/wat/darts/repositories/RoundRepository.java
+++ src/main/java/tim/wat/darts/repositories/RoundRepository.java
@@ -1,7 +1,6 @@
 package tim.wat.darts.repositories;
 
 
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.repository.CrudRepository;
 import tim.wat.darts.source.Contest;
@@ -14,8 +13,5 @@ import java.util.List;
 @Repository
 public interface RoundRepository extends CrudRepository<Round, Long> {
     ArrayList<Round> findAllByContestAndPlayer(Contest contest, Player player);
-    @Query("select c.id, r.amount, r.full_amount,r.photo_path,p.login from contest c " +
-            "left join contest_player player on c.id = player.contest_id " +
-            "join round r on c.id = r.contest_id where c.contest_name=?1")
     ArrayList<Round> findAllByContest(Contest contest);
 }
